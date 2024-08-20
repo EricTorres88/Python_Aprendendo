777b8db3d4df55d839f791bb15ef60bfa802ac11
José Eric Souza Torres <221.14.043@EDU.gruposete.local>
2024-08-19 22:08:27 -0300

    Comentarios

--- media.py
+++ media.py
@@ -1,6 +1,7 @@
 def media_aritmetica(n1, n2, n3):
     media = (n1 + n2 + n3) / 3
     print(f"Sua média aritmética é: {media:.2f}")
+    #A sintaxe :.2f dentro da média, retorna apenas duas casas decimais da média.
 
 n1 = float(input("Digite um número: "))
 n2 = float(input("Digite um segundo número: "))
